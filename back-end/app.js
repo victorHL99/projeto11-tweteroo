3f34e02b2ea92ecf0147216206ae3b0f814af8f6
victorHL99 <victor.lessa@estudante.ufla.br>
2022-04-22 14:17:29 -0300

    Get funcionando

--- back-end/app.js
+++ back-end/app.js
@@ -3,5 +3,18 @@ import cors from 'cors';
 
 const app = express();
 app.listen(5000, () => console.log('server started'));
+app.use(express.json());
 app.use(cors());
 
+const tweets = [];
+const users = [];
+
+
+
+
+//Get recebendo tweets
+app.get('/tweets', (req, res) => {
+	for(let i = 0; i <= 10; i++){
+		res.send(tweets);
+	}
+});

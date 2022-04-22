5493185ad4a04f7240731c074205a9ba244f193f
victorHL99 <victor.lessa@estudante.ufla.br>
2022-04-22 14:45:03 -0300

    Post funcionando mas imagem ainda nao está aparecendo

--- back-end/app.js
+++ back-end/app.js
@@ -6,19 +6,38 @@ app.listen(5000, () => console.log('server started'));
 app.use(express.json());
 app.use(cors());
 
-const tweets = [];
+const tweets = [
+	{
+		username: "bobesponja",
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+		tweet: "eu amo o hub"
+	},
+	
+];
 const users = [];
 
 
 app.post('/sign-up', (req, res) => {
 	const {username, avatar} = req.body;
-	users.unshift({username, avatar});
+
+	const usuario = {
+		"username": username,
+		"avatar": avatar
+	}
+
+	users.unshift(usuario);
 	res.send("OK");
 })
 
 app.post('/tweets', (req, res) => {
-	const {username, tweet} = req.body;
-	tweets.unshift(tweet);
+	const {username ,tweet} = req.body;
+
+	const tweetObj = {
+		"username": username,
+		"tweet": tweet
+	}
+
+	tweets.unshift(tweetObj);
 	res.send("OK");
 })
 

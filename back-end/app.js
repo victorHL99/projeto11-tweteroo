55dca998b6901c5ba6f93d9630d6ca74f3f7f666
victorHL99 <victor.lessa@estudante.ufla.br>
2022-04-25 16:08:47 -0300

    Arrumando a porta correta

--- back-end/app.js
+++ back-end/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-app.listen(5500, () => console.log('server started'));
+app.listen(5000, () => console.log('server started'));
 app.use(express.json());
 app.use(cors());
 

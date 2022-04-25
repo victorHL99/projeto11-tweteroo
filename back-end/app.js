dab9f3ba0e7a9d816c7fcacfb54a3337161a0ef3
victorHL99 <victor.lessa@estudante.ufla.br>
2022-04-25 16:03:12 -0300

    finalizado

--- back-end/app.js
+++ back-end/app.js
@@ -2,48 +2,73 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-app.listen(5000, () => console.log('server started'));
+app.listen(5500, () => console.log('server started'));
 app.use(express.json());
 app.use(cors());
 
-const tweets = [
-	{
-		username: "bobesponja",
-		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-		tweet: "eu amo o hub"
-	},
-	
-];
+const tweets = [];
 const users = [];
-
 
 app.post('/sign-up', (req, res) => {
 	const {username, avatar} = req.body;
-
+	
 	const usuario = {
 		"username": username,
 		"avatar": avatar
 	}
-
-	users.unshift(usuario);
+	
+	users.push(usuario);
 	res.send("OK");
 })
 
+// postando tweets
 app.post('/tweets', (req, res) => {
-	const {username ,tweet} = req.body;
+	const {avatar} = users
+	const {username,tweet} = req.body;
 
 	const tweetObj = {
 		"username": username,
+		"avatar": avatar,
 		"tweet": tweet
 	}
 
-	tweets.unshift(tweetObj);
+	tweets.push(tweetObj);
 	res.send("OK");
 })
 
-//Get recebendo tweets
 app.get('/tweets', (req, res) => {
-	for(let i = 0; i <= 10; i++){
-		res.send(tweets);
-	}
-});
+		const ultimosTweets = pegarTweets();
+		res.send(ultimosTweets);
+})
+;
+
+function pegarTweets() {
+    const ultimos10 = [];
+    
+    if (tweets.lenght === 0) {
+        return [];
+    } else
+    if (tweets.length > 10) {
+        for (let i = tweets.length - 10; i < tweets.length; i++) {
+            lastTweets(i);
+        }
+    }
+    else {
+        for (let i = 0; i < tweets.length; i++) { 
+            lastTweets(i);
+        }
+    }
+    
+    function lastTweets(i) {
+        const user = users.filter(element => element.username === tweets[i].username);
+
+        ultimos10.push(
+            {
+                "username": tweets[i].username,
+                "avatar": user[0].avatar,
+                "tweet": tweets[i].tweet
+            });
+    }
+
+    return ultimos10;
+}
